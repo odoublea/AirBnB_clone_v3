9d138fd9ea540157fc7c9d9770fa42caee7df1e9
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-08 23:00:11 +0100

    not found

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+    
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
