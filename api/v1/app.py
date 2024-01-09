53d99c9235960d8e650e8251beefe83675ea1118
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-09 10:12:35 +0100

    app doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask application set up"""""
+"""Flask application set up"""
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
@@ -12,14 +12,17 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not found"}), 404
-    
+
+
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
+
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -14,6 +16,17 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
+
+        @property
+        def password(self):
+            """Getter for password"""
+            return self._password
+
+        @password.setter
+        def password(self, value):
+            """Setter for password"""
+            self._password = hashlib.md5(value).encode()
+
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
@@ -23,6 +36,16 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+
+        @property
+        def password(self):
+            """Getter for password"""
+            return self._password
+
+        @password.setter
+        def password(self, value):
+            """Setter for password"""
+            self._password = hashlib.md5(value).encode()
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
