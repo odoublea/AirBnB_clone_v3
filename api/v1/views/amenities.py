8fedda89633c21f15a77d6ce774dcfd6e1dbfbfb
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-09 04:42:41 +0100

    user

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,56 @@
+from api.v1.views import app_views
+from flask import jsonify, request
+
+from models import storage
+from models.amenity import Amenity
+
+@app_views.route('/amenities')
+def amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    return jsonify(amenities)
+
+@app_views.route('/amenities/<amenity_id>')
+def amenity(amenity_id):
+    """Retrieves a Amenity objects"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(amenity.to_dict())
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a Amenity objects"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """Creates a Amenity objects"""
+    amenity_dict = request.get_json()
+    if amenity_dict is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if "name" not in amenity_dict:
+        return jsonify({"error": "Missing name"}), 400
+    amenity = Amenity(**amenity_dict)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates a Amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+    amenity_dict = request.get_json()
+    if amenity_dict is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    for key, value in amenity_dict.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
---
+++ api/v1/views/users.py
@@ -0,0 +1,58 @@
+from api.v1.views import app_views
+from flask import jsonify, request
+
+from models import storage
+from models.user import User
+
+@app_views.route('/users')
+def users():
+    """Retrieves the list of all User objects"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+@app_views.route('/users/<user_id>')
+def user(user_id):
+    """Retrieves a User objects"""
+    user = storage.get(User, user_id)
+    if user is None:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """Deletes a User objects"""
+    user = storage.get(User, user_id)
+    if user is None:
+        return jsonify({"error": "Not found"}), 404
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/users', methods=['POST'])
+def create_user():
+    """Creates a User objects"""
+    user_dict = request.get_json()
+    if user_dict is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if "email" not in user_dict:
+        return jsonify({"error": "Missing email"}), 400
+    if "password" not in user_dict:
+        return jsonify({"error": "Missing password"}), 400
+    user = User(**user_dict)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """Updates a User"""
+    user = storage.get(User, user_id)
+    if user is None:
+        return jsonify({"error": "Not found"}), 404
+    user_dict = request.get_json()
+    if user_dict is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    for key, value in user_dict.items():
+        if key not in ["id", "created_at", "updated_at", "email"]:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
