d8ae6b6fffbb101eeb871f22da86e2fb907e6670
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-09 10:37:58 +0100

    Flask app test

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,23 +2,33 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response, render_template, url_for
 from flask_cors import CORS, cross_origin
+from flasgger import Swagger
 from models import storage
 import os
 from werkzeug.exceptions import HTTPException
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
+swagger = Swagger(app)
 
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', '5000')
+port = os.getenv('HBNB_API_PORT', 5000)
 
-cors = CORS(app, resources={r"/*": {"origins": host}})
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r'/*': {'origins': host}})
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
 
+# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
     """
@@ -28,7 +38,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.errorhandler(404)
+@app.errorhandler(Exception)
 def global_error_handler(err):
     """
         Global Route to handle All Error Status Codes
@@ -44,8 +54,19 @@ def global_error_handler(err):
     return make_response(jsonify(message), code)
 
 
-if __name__ == '__main__':
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
+if __name__ == "__main__":
     """
     MAIN Flask App
     """
-    app.run(host=host, port=port, threaded=True)
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
+    app.run(host=host, port=port)
